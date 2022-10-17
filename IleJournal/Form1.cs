1813a773098b0623b55a0f6d96a35b308c234ba2
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-10-17 16:31:32 +0300

    datetime fixes needed

--- IleJournal/Form1.cs
+++ IleJournal/Form1.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.IO;
 using System.Net.Security;
 using System.Windows.Forms;
 
@@ -13,50 +14,65 @@ namespace IleJournal
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            //Insert timestamp to note
-            DateTime time = new DateTime();
-            time = DateTime.Now;
-            string week = Weekmethod(time).ToString();
-            string date = DateTime.Today.ToShortDateString();
+        { 
+            //Get dates
+            string week = JournalHelpers.Weekmethod(DateTime.Now).ToString();
+            //string date = DateTime.Today.ToShortDateString();
+            string date = DateTime.Today.AddDays(1).ToShortDateString();
             string weekday = DateTime.Today.DayOfWeek.ToString();
 
-            //Koosta teksti yhdeksi stringiksi
+            //get old data if the weekly file exists
+            if (File.Exists(@"C:\Users\ilari\source\repos\IleJournal\Entrys\" + week + ".rtf"))
+                richTextBox1.LoadFile(@"C:\Users\ilari\source\repos\IleJournal\Entrys\" + week + ".rtf");
+                     
+            //write timestamp and concatinate if stamp doesn´t exist
+            string stamp= TimeStamp( week, date, weekday);
+            
+            
+                richTextBox1.AppendText(stamp);
 
-            string Heading= "Week number: "+week + "\n \n" + date +" " + weekday+"\n" +" ";
-
-            richTextBox1.Text = Heading;
+            //Move cursor to end
             richTextBox1.Select(richTextBox1.Text.Length - 1, 0);
+        }
+
+        private string TimeStamp(string week, string date, string weekday)
+        {
+                    //Koosta teksti yhdeksi stringiksi
+            //Heading määräytyy sen mukaan, onko tekstitedostoa olemassa
+            string InitialHeading = "Week number: " + week + "\n \n" + date + " " + weekday + "\n" + " ";
+            string DailyHeading = "\n------------------------\n" +date + " " + weekday + "\n" + " ";
+
+            if (File.Exists(@"C:\Users\ilari\source\repos\IleJournal\Entrys\" + week + ".rtf"))
+            {
+                if (richTextBox1.Find(DailyHeading) != -1)
+                    return DailyHeading;
+                else
+                    return " ";
+            }
+            else
+                return InitialHeading;
 
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-           
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string message = "det wörkkar";
+
+            //make a path for weeknumber
+            string fileName = JournalHelpers.Weekmethod(DateTime.Now).ToString();
+            string fullPath = @"C:\Users\ilari\source\repos\IleJournal\Entrys\" + fileName +".rtf";
+
+            richTextBox1.SaveFile(fullPath, RichTextBoxStreamType.RichText);
+
+            string message = "Saved!";
             MessageBox.Show(message);
-
-        }
-    public static int Weekmethod (DateTime time)
-    {
-        // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
-        // be the same week# as whatever Thursday, Friday or Saturday are,
-        // and we always get those right
-        DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
-        if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
-        {
-            time = time.AddDays(3);
         }
 
-        // Return the week of our adjusted day
-        return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-    // This presumes that weeks start with Monday.
-    // Week 1 is the 1st week of the year with a Thursday in it.
-    }
-    }
 
+
+    }
 }
---
+++ IleJournal/JournalHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace IleJournal
+{
+    internal static class JournalHelpers
+    {
+        public static int Weekmethod(DateTime time)
+        {
+            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
+            // be the same week# as whatever Thursday, Friday or Saturday are,
+            // and we always get those right
+            DayOfWeek day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            // Return the week of our adjusted day
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            // This presumes that weeks start with Monday.
+            // Week 1 is the 1st week of the year with a Thursday in it.
+        }
+    }
+}
