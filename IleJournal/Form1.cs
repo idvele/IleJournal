8cfcb58bdae51e7c96878025c9d0fc04a1ac2e82
Ilari Katajamäki <ilari.katajamaki1@gmail.com>
2022-10-24 11:08:31 +0300

    making the db-connection

--- IleJournal/Form1.cs
+++ IleJournal/Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using System.Globalization;
 using System.IO;
 using System.Net.Security;
@@ -12,24 +13,22 @@ namespace IleJournal
             InitializeComponent();
 
         }
-
+        //AddDays(1). tester add to date
         private void Form1_Load(object sender, EventArgs e)
         { 
             //Get dates
             string week = JournalHelpers.Weekmethod(DateTime.Now).ToString();
             //string date = DateTime.Today.ToShortDateString();
-            string date = DateTime.Today.AddDays(1).ToShortDateString();
+            string date = DateTime.Today.ToShortDateString();
             string weekday = DateTime.Today.DayOfWeek.ToString();
 
             //get old data if the weekly file exists
             if (File.Exists(@"C:\Users\ilari\source\repos\IleJournal\Entrys\" + week + ".rtf"))
                 richTextBox1.LoadFile(@"C:\Users\ilari\source\repos\IleJournal\Entrys\" + week + ".rtf");
-                     
+
             //write timestamp and concatinate if stamp doesn¥t exist
             string stamp= TimeStamp( week, date, weekday);
-            
-            
-                richTextBox1.AppendText(stamp);
+            richTextBox1.AppendText(stamp);
 
             //Move cursor to end
             richTextBox1.Select(richTextBox1.Text.Length - 1, 0);
@@ -39,12 +38,12 @@ namespace IleJournal
         {
                     //Koosta teksti yhdeksi stringiksi
             //Heading m‰‰r‰ytyy sen mukaan, onko tekstitedostoa olemassa
-            string InitialHeading = "Week number: " + week + "\n \n" + date + " " + weekday + "\n" + " ";
-            string DailyHeading = "\n------------------------\n" +date + " " + weekday + "\n" + " ";
+            string InitialHeading = "Week number: " + week + "\n \n" + date + " " + weekday + "\n\n" + " ";
+            string DailyHeading = "\n------------------------\n" +date + " " + weekday + "\n\n" + " ";
 
             if (File.Exists(@"C:\Users\ilari\source\repos\IleJournal\Entrys\" + week + ".rtf"))
             {
-                if (richTextBox1.Find(DailyHeading) != -1)
+                if (richTextBox1.Find(date) == -1)
                     return DailyHeading;
                 else
                     return " ";
@@ -66,13 +65,38 @@ namespace IleJournal
             string fileName = JournalHelpers.Weekmethod(DateTime.Now).ToString();
             string fullPath = @"C:\Users\ilari\source\repos\IleJournal\Entrys\" + fileName +".rtf";
 
+            //save for current weeknumber
             richTextBox1.SaveFile(fullPath, RichTextBoxStreamType.RichText);
 
             string message = "Saved!";
             MessageBox.Show(message);
         }
 
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection cnn;
+
+            connectionString = @"Data Source=DESKTOP-FJGGHA7\MSSQLSERVER01;Initial Catalog=JournalDb;Integrated Security=True;";
+
+            cnn= new SqlConnection(connectionString);
+
+            cnn.Open();
+            //tehd‰‰n haku databasesta ja printataan se richtextboxiin
+            
+            string sql = "Select Journal_text from testitable";
+            SqlCommand command = new SqlCommand(sql, cnn);
+            SqlDataReader reader = command.ExecuteReader();
+            String Output = "";
+
+            while (reader.Read())
+            {
+                Output = Output + reader.GetValue(0);
+            }
 
 
+            richTextBox1.Text=Output;
+            cnn.Close();
+        }
     }
-}
+}//Tsekkaa t‰‰ https://www.guru99.com/c-sharp-access-database.html
